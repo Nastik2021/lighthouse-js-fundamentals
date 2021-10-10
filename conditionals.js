a256de2e14f1054151a6f35a6464e019b789a946
Anastassia <nastiatsv@gmail.com>
2021-10-09 23:00:18 -0700

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -23,3 +23,8 @@ if (isCitizen && age > 18) {
 if (temperature < -40 || temperature > 40) {
   console.log("Maybe going outside isn't such a great idea…");
 }
+
+//another example
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
